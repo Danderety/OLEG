23b25f1e57086146adc0e8ce9c4a0e01b3ff487d
Danderety <kirillsmerkovic@gmail.com>
2025-05-28 20:49:30 +0300

    Добавил библиотеки

--- notes/NOTES.py
+++ notes/NOTES.py
@@ -1,1 +1,15 @@
-print("hello world")
+import os
+import sys
+import sqlite3
+import platform
+import shutil
+import configparser
+
+from datetime import datetime
+from PyQt6.QtWidgets import (QApplication, QMainWindow, QPushButton,
+                             QFileDialog, QColorDialog, QFontDialog,
+                             QLabel, QVBoxLayout, QHBoxLayout, QWidget, QLineEdit, QTableWidget,
+                             QTableWidgetItem, QMessageBox, QMenuBar, QMenu, QHeaderView, QMenu, QInputDialog)
+from PyQt6.QtCore import QTimer, QTime, Qt
+from PyQt6.QtGui import QColor, QFont, QDesktopServices, QAction
+from PyQt6.QtCore import QUrl
