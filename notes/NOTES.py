4c415ec3500fdf8ee60e875d10302e955fb42c30
Bezotca48 <oleg.vinogradov9032010@yandex.ru>
2025-05-28 21:38:04 +0300

    создал базу данных

--- notes/NOTES.py
+++ notes/NOTES.py
@@ -13,3 +13,31 @@ from PyQt6.QtWidgets import (QApplication, QMainWindow, QPushButton,
 from PyQt6.QtCore import QTimer, QTime, Qt
 from PyQt6.QtGui import QColor, QFont, QDesktopServices, QAction
 from PyQt6.QtCore import QUrl
+DB_FILE="notes.db"
+attachments_dir="attachments"
+os.makedirs(attachments_dir, exist_ok=True)
+class NotesApp(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Заметки")
+        self.setGeometry(200,200,800,600)
+        self.load_theme_preference()
+        self.init_db()
+        self.init_ui()
+        self.load_notes()
+        self.schedule_notification()
+        self.apply_theme()
+    def init_db(self):
+        self.conn=sqlite3.connect(DB_FILE)
+        self.cursor=self.conn.cursor()
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS notes (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,  # Уникальный ID
+                timestamp TEXT,                       # Время создания
+                content TEXT,                         # Текст заметки
+                attachment TEXT                       # Путь к вложению
+            )
+        """)
+        self.conn.commit()
+
+            
